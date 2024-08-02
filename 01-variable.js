da1cc71dcad03102e1a30f9015c629e474d804f0
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-02 20:08:07 +0530

    javascript variables
    javascript variables

---
+++ 01-variable.js
@@ -0,0 +1,13 @@
+// variable Declaration
+
+var firstName;
+console.log(firstName);
+// variable Initialization
+firstName="madhuri"
+console.log(firstName);
+// varible Declaration and variable Initiazation
+var lastName="jagtap";
+console.log(lastName);
+// variable update
+firstName="mj";
+console.log(firstName);
