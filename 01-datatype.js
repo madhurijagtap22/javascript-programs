d2fac8ff42c401a333ca1b3bcf6349cf2a9d9020
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-03 07:18:48 +0530

    datatype
    datatype

---
+++ 01-datatype.js
@@ -0,0 +1,47 @@
+
+console.log("===================number datatype=============");
+var age=22;
+console.log("age is:",age);
+var ageTypeOf=typeof age;
+console.log("Datatype of age is:",ageTypeOf);
+
+console.log("================== string datatype===========");
+var city="pune";
+console.log("city is:",city);
+var cityTypeOf=typeof city;
+console.log("Datatype of city is:",cityTypeOf);
+
+console.log("==========boolean datatype========");
+var isMarried=true;
+console.log("isMarried:",isMarried);
+var isMarriedTypeOf=typeof isMarried;
+console.log("isMarried type of:",isMarriedTypeOf);
+
+console.log("============special Datatype:undefined============");
+  var country;
+  console.log("country value is:",country);
+  var countryDatatypeOf=typeof country;
+  console.log("country datatype is:",countryDatatypeOf);
+
+  console.log("============Null Datatype===========");
+  var village=null;
+  console.log("value of village is:",village);
+  var villageDatatypeOF=typeof village;
+  console.log("Datatype of village is:",villageDatatypeOF);
+  
+  
+  
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
