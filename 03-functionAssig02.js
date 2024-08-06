f56f0848ead3b60d09faef4bd67118f6e6dbb1f6
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-06 21:25:52 +0530

    function assignment no.02
    function assignment no.02

--- 03-functionAssig02.js
+++ 03-functionAssig02.js
@@ -1,12 +1,8 @@
+
 function bankDeails( bankName,accountNum,location,pinCode){
     
     console.log("my bank name is:",bankName  , "   account number is:",accountNum, "  location:",location  ,"  pinCode:",pinCode,);
     
-
-
-
-
-
 }
 bankDeails("CITI Bank",3456782345,"pune",431202);
 bankDeails("Axis Bank",785682345,"Mumbai",441202);
