babbb6a0f4603887f7b98a2bdc9c4b72c9f5e0f1
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-07 22:13:17 +0530

    type-conversion
    type-conversion

---
+++ 05-NaN.js
@@ -0,0 +1,68 @@
+
+var n1=0;
+var n2=0;
+var result=n1/n2;
+console.log(result);
+
+console.log(`==============================`);
+var n3=10;
+var n4;
+console.log(n3-n4);
+
+console.log(`===========Uses cases of + oprrator============`);
+console.log(`1.Addition: ${4+5}`);
+
+var s1="Hello";
+var s2="Good Morning"
+console.log(`2.concatenation :${s1+s2}`);
+
+console.log(`3.conversion`);
+var n5="50"
+console.log(`type of n5 : ${typeof n5}`);
+
+var result=+n5;
+console.log(`type of n5 : ${typeof result}`);
+
+console.log(`==============================`);
+var n6;
+console.log(`type of n6 :${typeof n6}`);
+var result=+n6;
+console.log(`type of result : ${result},${typeof result}`);
+
+console.log(`=================Implicit Conversion=================`);
+console.log('5'+2);
+
+console.log(10+true);
+
+console.log(10+false);
+
+console.log(`=========string to number=================`);
+var n1="10";
+var result=+n1;
+var result=Number(n1);
+console.log(`result value ${result} and its type is ${typeof result}`);
+
+console.log(`=====================Number to String============`);
+var n2=20;
+var result=n2.toString();
+console.log(`n2 value ${n2} and its type is ${typeof n2}`);
+console.log(`result value is ${result} and its type is ${typeof result}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
