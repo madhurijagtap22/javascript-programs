6cf8229556367dad1b8741e8f2b0d44681ada534
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-07 11:48:49 +0530

    operator-assignment
    operator-assignment

--- 04-operator-assignment.js
+++ 04-operator-assignment.js
@@ -4,3 +4,22 @@ var n2 = 5;
 n2+=20;// n2 = n2+20
 console.log(`Compound Addition is: ${n2}`);
 
+n2-=5;
+console.log(`Compount Subtraction is : ${n2}`);
+
+n1*=10;
+console.log(`Compount Multiplication : ${n1}`);
+
+n1/=5;
+console.log(`Compount Division : ${n1}`);
+
+n1%=3;
+console.log(`Compount Modulus : ${n1}`);
+
+
+
+
+
+
+
+
