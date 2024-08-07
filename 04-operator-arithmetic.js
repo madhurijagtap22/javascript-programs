d7a022528767cb3e0842f385c05668972a7536b3
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-07 11:36:19 +0530

    operator-arithmetic
    operator-arithmetic

--- 04-operator-arithmetic.js
+++ 04-operator-arithmetic.js
@@ -1,12 +1,16 @@
 console.log(`======== Arithmetic Operators =========`);
 var n1 = 10;
 var n2 = 5;
+
 var result = n1 + n2;
 console.log(`Addition is : ${result}`);
+
 var result = n1 - n2;
 console.log(`Subtraction is : ${result}`);
+
 var result = n1 * n2;
 console.log(`Multiplication is : ${result}`);
+
 var result = n1 / n2;
 console.log(`Division is : ${result}`);
 
