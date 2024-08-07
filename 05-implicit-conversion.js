8db038c994976437d136c847ef9c58c6a0b0a6a0
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-07 22:23:50 +0530

    implicit-conversion
    implicit-conversion

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,20 @@
+result ='3'+2;
+console.log(result);
+console.log(`============================`);
+
+result='3'+ true;
+console.log(result);
+console.log(`================================`);
+
+result='3' + undefined;
+console.log(result);
+console.log(`======================`);
+
+result='3'+null;
+console.log(result);
+
+
+
+
+
+
