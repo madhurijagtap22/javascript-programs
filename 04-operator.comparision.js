26390e8e9ba42730e586039778c552b4fc6a754c
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-07 12:08:01 +0530

    operator-comparison
    operator-comparison

--- 04-operator.comparision.js
+++ 04-operator.comparision.js
@@ -1,9 +1,24 @@
-var n1 = "50";
+var n1 = 60;
 var n2 = 50;
-//var result = n1 == n2;
-console.log(n1 == n2);
-console.log(n1 === n2);
+console.log(`"n1" is equal to "n2" : ${n1==n2}`);
 
-console.log(n1!=n2);
+console.log(`"n1" is not equal to "n2" : ${n1!=n2}`);
+
+console.log(`"n1" is greater than "n2" : ${n1>n2}`);
+
+console.log(`"n1" is less than "n2" : ${n1<n2}`);
+
+console.log(`"n1" is greater than or  equal to "n2" : ${n1>=n2}`);
+
+console.log(`"n1" is less than equal to "n2" : ${n1<=n2}`);
+
+console.log(`"n1" is strict equal to "n2" : ${n1===n2}`);
+
+
+
+
+
+
+
 
 
