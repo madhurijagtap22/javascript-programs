cc1d5919d6f579de6582738d0b1ab29763fb5822
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-10 12:03:08 +0530

    for-loop-assignment01
    for-loop-assignment01

---
+++ 06-for-loop-assig01.js
@@ -0,0 +1,45 @@
+
+console.log(`=================print numbers from 5 to 15=======================`);
+for (let i = 5; i <=15; i=i+1) {
+    console.log(i);   
+}
+
+console.log(`===================print numbers from 50 to 40 ====================`);
+for (let i = 50; i>=40; i=i-1) {
+    console.log(i);    
+}
+
+console.log(`================print the first 15 odd number===================`);
+for (let i = 1; i<=30; i=i+2) {
+    console.log(i);   
+}
+
+console.log(`=================print the first 10 even numbers================`);
+for (let index = 0; index <=20; index=index+2) {
+    console.log(index);    
+}
+
+console.log(`=================print table of 5 ====================`);
+for (let index = 5; index <=50; index=index+5) {
+    console.log(index);   
+}
+
+console.log(`==================print the table of 10 ======================`);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);    
+}
+
+console.log(`====================print the table of 10 in reverse order ===================`);
+for (let index = 100; index>=10; index=index-10) {
+    console.log(index);    
+}
+
+
+
+
+
+    
+
+    
+
+
