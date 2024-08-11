f509318efc59e7da1c7c80ee8140e4663d3d7f3a
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-11 12:33:59 +0530

    while-loop-assignment
    while-loop-assignment

---
+++ 06-while-loop-assig.js
@@ -0,0 +1,45 @@
+console.log(`=================print numbers from 5 to 15==================`);
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i=i+1;   
+}
+
+console.log(`=================print numbers from50 to 40==================`);
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i=i-1;    
+}
+
+console.log(`==================print the first 15 odd number===========`);
+var i=1;
+while (i<=30) {
+    console.log(i);
+    i=i+2;   
+}
+
+console.log(`===================print the first 10 even number=====`);
+var i=0;
+while (i<=20) {
+    console.log(i);
+    i=i+2;   
+}
+
+console.log(`==============print the table of 5 =============`);
+var i=5;
+while (i<=50) {  
+    console.log(i);
+    i=i+5   
+}
+
+console.log(`============print the table of 10 in reverse order==========`);
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i=i-10
+}
+
+
+
+
