2d32a6d909719d2ac7dd852755fad1e3a1e45a5d
madhurijagtap22 <madhurijagtap422@gmail.com>
2024-08-24 22:15:19 +0530

    strict-mode
    strict-mode

---
+++ 13-strict-mode.js
@@ -0,0 +1,15 @@
+`use strict`;
+
+// city = "Pune";
+// console.log(city);
+
+// let pin = 411057;
+// delete pin;
+
+// student = {
+//     fullName: "Jenny Sunny",
+//     age: 20
+// }
+
+// delete student;
+
